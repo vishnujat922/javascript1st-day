090fa03a03a3fe30445a363f500477c3710ca72e
vishnujat922 <vishnujat922@gmail.com>
2023-03-05 17:48:20 +0530

    Java script 1st Days Basic to  Starting

---
+++ script.js
@@ -0,0 +1,6 @@
+// statement 1
+var x = 10;
+var y = 20;
+console.log(x - y);
+window.alert(x - y);
+console.log('Hello Everyone Iam Vishnu Jaat I will starting Frontend Development Course By Oklabs' )
